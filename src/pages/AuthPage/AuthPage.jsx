e5ac786f457554813a7a92d9042075c672ad591e
jchung501 <johnchung2011@gmail.com>
2022-05-03 22:11:21 -0500

    Minor styling changes made

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -11,7 +11,7 @@ export default function AuthPage({ setUser }) {
     <main className={styles.AuthPage}>
       <div>
         <Logo />
-        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up' : 'Login'}</h3>
       </div>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
     </main>
