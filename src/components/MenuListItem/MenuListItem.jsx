302ec397aed93bca448c9db899de903d789b5dda
jchung501 <johnchung2011@gmail.com>
2022-05-05 23:43:52 -0500

    made btn-add class, coded btn-add hover, btn-add, and a.button:Hover, change button class in MenuListItem to btn-add, made li:hover color to be skobeloff

--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -7,7 +7,7 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
       <div className={styles.name}>{menuItem.name}</div>
       <div className={styles.buy}>
         <span>${menuItem.price.toFixed(2)}</span>
-        <button className="btn-sm" onClick={() => handleAddToOrder(menuItem._id)}>
+        <button className="btn-add" onClick={() => handleAddToOrder(menuItem._id)}>
           Add
         </button>
       </div>
