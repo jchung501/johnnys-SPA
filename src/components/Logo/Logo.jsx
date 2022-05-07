6eb1c4476193fc5894eb42c7caba1ee9aeb67b74
jchung501 <johnchung2011@gmail.com>
2022-05-07 09:04:22 -0500

    minor changes, deploying to heroku

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import styles from './Logo.module.css';
 
 export default function Logo() {
 return (
-  <div>
+  <div className={styles.img}>
     <img src="https://i.imgur.com/Nx3bqH5.png"/>
   </div>
 );
